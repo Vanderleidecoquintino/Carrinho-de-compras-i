1f180382376bb352fcc4f5a409d8cfe2766a92e8
Vanderleidecoquintino <108231938+Vanderleidecoquintino@users.noreply.github.com>
2026-09-17 17:50:20 -0300

    Update app.py

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ produtos = [
     {"nome":"Chocolate Harald 500g","preco":59.90,"ean":"7896063800011","yolo":["box","book"]},
     {"nome":"Suco Subello 200ml","preco":1.49,"ean":"7898951000015","yolo":["box"]},
     {"nome":"[BULNEZ] Esponja 3un","preco":2.49,"ean":"7898915120039","yolo":["box"]},
-    {"nome":"Feijão Kicaldo 1kg","preco":7.50,"ean":"7896101000013","yolo":["box","book"]},
+    {"nome":"sacola","preco":0,30,"ean":"7899739503929","yolo":["box","book"]},
 ]
 
 def bip():
